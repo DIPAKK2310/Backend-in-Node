d2fbf1df0a019c984ec204b9c513efaddb1c8aa5
DIPAKK2310 <dipakkhare217@gmail.com>
2025-08-10 17:44:52 +0530

    Updated files

---
+++ Nginx/server.js
@@ -0,0 +1,11 @@
+import { createServer } from 'http';
+import fs from 'fs';
+import path from 'path';
+
+const PORT = 8000;
+
+const server = createServer()
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
