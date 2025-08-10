bfc178ba44460d755707ab9811dc120d9b176b4b
DIPAKK2310 <dipakkhare217@gmail.com>
2025-08-10 17:55:27 +0530

    Again readme update

--- Nginx/server.js
+++ Nginx/server.js
@@ -4,8 +4,10 @@ import path from 'path';
 
 const PORT = 8000;
 
-const server = createServer()
+const server = createServer((req,res) =>{
 
+}
+);
 server.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
